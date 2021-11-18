3ada272e9d743dd1eb7e526352d9cf3f59355d7b
mjhd <mjhd.devlion@gmail.com>
2021-11-18 13:50:40 +0900

    fix incorrect import path

--- tools/analyzer_plugin/bin/plugin.dart
+++ tools/analyzer_plugin/bin/plugin.dart
@@ -1,6 +1,6 @@
 import 'dart:isolate';
 
-import 'package:flutter_hooks_lint_plugin/plugin.dart';
+import 'package:flutter_hooks_lint_plugin/flutter_hooks_lint_plugin.dart';
 
 void main(List<String> args, SendPort sendPort) {
   start(args, sendPort);
